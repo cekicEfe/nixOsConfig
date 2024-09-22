e1be08a813d03c42295e914815a9a28a48a3aa2a
cekicEfe <efecekic0@gmail.com>
2024-09-22 19:29:38 +0300

    tweaked nixvim

--- modules/importprograms/nixvim.nix
+++ modules/importprograms/nixvim.nix
@@ -10,32 +10,5 @@ in{
 	programs.nixvim = {
 		enable = true;
 		defaultEditor = true;
-
-		plugins = {
-
-		transparent = {
-			enable = true;
-		};
-		treesitter = {
-		    enable = true;
-			
-		    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-		      bash
-		      cpp
-		      json
-		      lua
-		      make
-		      markdown
-		      nix
-		      regex
-		      toml
-		      vim
-		      vimdoc
-		      xml
-		      yaml
-		    ];
-		  };
-
-		};
 	};
 }
